e8efd9315e16e116e0263b85c6c252ccb82aa353
kingaGniedziejko <47779092+kingaGniedziejko@users.noreply.github.com>
2021-01-02 19:30:06 +0100

    hide menu

--- docs/index.js
+++ docs/index.js
@@ -6,3 +6,15 @@ function responsiveMenu() {
          x.className = "menu";
      }
 }
+
+function hideMenu(id) {
+    var x = document.getElementById("menu");
+    if (x.className !== "menu") {
+        x.className = "menu";
+    }
+
+    // var menuLinks = document.getElementsByClassName("menu-link");
+    // for (var i = 0; i < menuLinks.length; i++) {
+    //     menuLinks[i].className = "menu-link";
+    // }
+}
